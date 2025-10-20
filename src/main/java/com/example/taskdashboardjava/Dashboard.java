7eb3094ec70f3c8105642f04d184676bbe5c37d5
GRedex <randula30085@gmail.com>
2025-10-20 13:58:41 +0530

    Added CSS class and scene builder elements

--- src/main/java/com/example/taskdashboardjava/Dashboard.java
+++ src/main/java/com/example/taskdashboardjava/Dashboard.java
@@ -3,23 +3,40 @@ package com.example.taskdashboardjava;
 //javaFx imports
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 //java util imports
 import java.io.IOException;
+import java.util.Objects;
 
 public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
-        Image icon = new Image("Icon.png");
-        stage.getIcons().add(icon);
-        stage.setTitle("Task Dashboard");
-        stage.setScene(scene);
-        stage.show();
+       try {
+           //get root if the file is not null
+           Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+           Scene scene = new Scene(root);
+
+           //If the css file is used in more than one scene
+           String cssFile = Objects.requireNonNull(this.getClass().getResource("/Application.css")).toExternalForm();
+           scene.getStylesheets().add(cssFile);
+
+           //if it is only having one css location
+           //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Application.css")).toExternalForm());
+
+           //setting the Image Icon for the window
+           Image icon = new Image("Icon.png");
+           stage.getIcons().add(icon);
+           stage.setTitle("Task Dashboard");
+           stage.setScene(scene);
+           stage.show();
+       }
+       catch (Exception e){
+           e.printStackTrace();
+       }
     }
 
 }
